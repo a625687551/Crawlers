e5fab8bece5e4f6ca13f784114a0d32108eb67f9
jianfeng.wang <jianfeng.wang@maixunbytes.com>
2018-03-15 10:31:26 +0800

    change item

--- CompanyCrawler/CompanyCrawler/items.py
+++ CompanyCrawler/CompanyCrawler/items.py
@@ -9,6 +9,50 @@ import scrapy
 
 
 class CompanycrawlerItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+        # define the fields for your item here like:
+        # 企业名字
+        name = scrapy.Field()
+        # 企业电话
+        phone = scrapy.Field()
+        # 企业邮箱
+        mail = scrapy.Field()
+        # 企业网页地址
+        net_addr = scrapy.Field()
+        # 企业地址
+        addr = scrapy.Field()
+        # 企业描述
+        desc = scrapy.Field()
+        # 法人
+        faren = scrapy.Field()
+        # 拥有的公司
+        owner_company = scrapy.Field()
+        # 注册资本
+        register_money = scrapy.Field()
+        # 注册时间
+        register_time = scrapy.Field()
+        # 公司状态
+        company_state = scrapy.Field()
+        # 工商注册代码
+        registration_num = scrapy.Field()
+        # 组织机构代码
+        organization_num = scrapy.Field()
+        # 统一信用代码
+        credit_num = scrapy.Field()
+        # 公司类型
+        company_type = scrapy.Field()
+        # 纳税人识别号
+        taxpayer_num = scrapy.Field()
+        # 行业
+        ndustry = scrapy.Field()
+        # 营业期限
+        bussiness_time = scrapy.Field()
+        # 核准时间
+        check_time = scrapy.Field()
+        # 登记机关
+        register_office = scrapy.Field()
+        # 英文名字
+        company_eng = scrapy.Field()
+        # 注册地址
+        register_addr = scrapy.Field()
+        # 经营范围
+        manage_orange = scrapy.Field()
---
+++ CompanyCrawler/CompanyCrawler/spiders/tianyancha.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# import scrapy
+import logging
+
+from functools import reduce
+from scrapy import Spider
+from scrapy import Request
+from urllib.parse import quote_plus
+
+from CompanyCrawler.CompanyCrawler.items import CompanycrawlerItem
+
+logger = logging.getLogger(__file__)
+logger.setLevel(logging.INFO)
+
+
+class TianyanSpider(Spider):
+    name = 'tianyan'
+    allowed_domains = ['tianyancha.com']
+    key_list = ["批发零售", "餐饮", "住宿", "建材", "商贸", "美容院", "艾灸", "教育培训"]
+    base_url = "https://xingtai.tianyancha.com/search/p{pg}?key={kw}"
+
+    def start_requests(self):
+        for key in self.key_list:
+            for pg in range(1, 6):
+                list_url = self.base_url.format(pg=pg, kw=quote_plus(key))
+                yield Request(url=list_url, callback=self.parse_list)
+
+    def parse_list(self, response):
+        content = response.xpath('//div[@class="search_right_item ml10"]/div/a/@href').extract()
+
+        for cell in content:
+            yield Request(url=cell, callback=self.parse_detail)
+
+    def parse_detail(self, response):
+        pass
