907dec812f3cf7c672b9a5483f5b2ede43226cc0
jianfeng.wang <jianfeng.wang@maixunbytes.com>
2019-02-22 18:50:00 +0800

    july question bank

---
+++ july_question_bank/july_questions.py
@@ -0,0 +1,22 @@
+# coding:utf-8
+
+import requests
+
+
+class july_question(object):
+    def __init__(self):
+        self.question_list = []
+        self.file_path = "question.txt"
+
+    def get_question_list(self, url):
+        pass
+
+    def get_question_info(self, url):
+        pass
+
+    def save_question(self, content):
+        pass
+
+
+if __name__ == '__main__':
+    pass
